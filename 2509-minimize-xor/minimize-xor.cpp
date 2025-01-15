ce791840b7870d5e4860ce1712545f6e490e8e5a
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-01-15 21:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (40.19%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int count = __builtin_popcount(num2);
+
+        int res = 0;
+        // Cancel set bits from highest to lowest weight
+        for(int i=31;i>=0 and count;i--){
+            if(num1 & (1<<i)){
+                count--;
+                res += (1<<i);
+            }
+        }
+
+        // Set bits lowest to highest weight
+        for(int i=0;i<32 and count;i++){
+            if((num1 & (1<<i))==0){
+                count--;
+                res += (1<<i);
+            }
+        }
+
+        return res;
+    }
+};
+
+
+// TC = O(logN)
+// SC = O(1)
