6626d21dcc776d6bad08d541ae523e1ef1f2a15d
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-01-20 23:23:15 +0530

    Time: 3 ms (97.87%) | Memory: 133.8 MB (91.77%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    #define pii pair<int,int>
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+
+        // Step1 Create lookup for each value in matrix
+        vector<pii>lookup(m*n+1); // Stores x,y coordinates for each element in matrix
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                lookup[mat[i][j]]=make_pair(i,j);
+
+                // Step2 Find earliest row or col painted
+                vector<int>row_count(m,0);
+                vector<int>col_count(n,0);
+
+                for(int i=0;i<m*n;i++){
+                    pii coordinate = lookup[arr[i]];
+                    int x = coordinate.first;
+                    int y = coordinate.second;
+
+                    row_count[x]++;
+                    col_count[y]++;
+                    if(row_count[x]==n or col_count[y]==m)
+                    return i;
+                }
+                return 0;
+            }
+};
+
+
+// TC = O(MN)
+// SC = O(MN)
