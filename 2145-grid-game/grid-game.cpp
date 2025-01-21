52812dbd88ca377dba5f4588afdf2f0ec3410bd5
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-01-21 23:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 66.7 MB (61.54%) - LeetSync

---
+++ 2145-grid-game/grid-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    #define ll long long
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int n=grid[0].size();
+        if(n<2) return 0;
+
+        ll top_sum = accumulate(grid[0].begin(),grid[0].end(),0LL);
+        ll bottom_sum = 0;
+
+        /* Robo-2 can only cover either top or bottom,
+           Since, it too want to maximize it's sum value,
+           so, take max(top_sum,bottom_sum);
+
+           */
+
+           ll min_sum = LONG_MAX;
+           for(int pp=0;pp<n;pp++){
+            top_sum -= grid[0][pp];
+            min_sum = min(min_sum,max(top_sum,bottom_sum));
+            bottom_sum += grid[1][pp];
+           }
+
+           return min_sum;
+
+
+    }
+};
+
+// TC = O(N)
+// SC = O(1)
