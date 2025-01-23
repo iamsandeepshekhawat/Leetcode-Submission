0c7147231ed7559014416ceb8934d837a9446c93
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-01-23 23:34:30 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (39.90%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int> row_count(m,0);
+        vector<int> col_count(n,0);
+
+        // Step1 Count Frequency of each row & col
+        int total_servers=0;
+        for(int i=0;i<m;i++)
+           for(int j=0;j<n;j++)
+           if(grid[i][j]){
+            row_count[i]++;
+            col_count[j]++;
+            total_servers++;
+           }
+
+        // Step2 Check non-communicating servers
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+            if(grid[i][j] and (row_count[i]==1 and col_count[j]==1))
+                total_servers--;
+
+                return total_servers;
+
+
+
+    }
+};
+
+// TC = O(MN)
+// SC = O(M+N)
