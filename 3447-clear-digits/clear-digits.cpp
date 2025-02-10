c3c255bf1ca54241929db2ebb4874bb658f69819
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-02-10 23:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.42%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res;
+        for(char c: s){
+            if(c>='0' and c<='9')  res.pop_back();
+               else                res.push_back(c);
+        }
+
+        return res;
+        
+    }
+};
+
+// TC = O(N)
+// SC = O(N)
