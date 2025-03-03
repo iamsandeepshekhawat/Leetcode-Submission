2b884f9b8c95c371eca599fe09683dc0e8bba8a8
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-03-03 23:56:20 +0530

    Time: 1 ms (93.24%) | Memory: 127.7 MB (77.25%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n=nums.size();
+        vector<int>res(n);
+
+        // Throw ele<pivot in left basket and ele>pivot in right basket
+        int left=0,right=n-1;
+        int left_res=0,right_res=n-1;
+        while(left<n){
+            if(nums[left]<pivot)     res[left_res++]=nums[left];
+            if(nums[right]>pivot)    res[right_res--]=nums[right];
+            left = left + 1;
+            right = right - 1;
+
+        }
+        // Now fill pivot ele in the middle part
+        while(left_res<=right_res)
+              res[left_res++]=pivot;
+        return res;
+    }
+};
+
+// TC = O(N)
+// SC = O(N)
