d7972ce085b7bbaf630247151dad97e55f407522
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-03-17 23:00:05 +0530

    Time: 11 ms (5.86%) | Memory: 18.2 MB (9.58%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int>remaining;
+        for(int ele: nums){
+            if(remaining.count(ele))    remaining.erase(ele);
+            else                        remaining.insert(ele);
+        }
+
+        return remaining.size()==0;
+    }
+};
+
+// TC = O(N)
+// SC = O(N)
