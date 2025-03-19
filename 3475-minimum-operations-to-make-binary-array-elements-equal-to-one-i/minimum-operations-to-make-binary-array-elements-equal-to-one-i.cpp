d365a7a072290cf7157f707f74568a08f92edb24
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-03-19 23:17:17 +0530

    Time: 12 ms (26.22%) | Memory: 140 MB (74.50%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    void flipWindow(vector<int>& nums,int pos){
+        nums[pos]^=1;
+        nums[pos+1]^=1;
+        nums[pos+2]^=1;
+    }
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int operations = 0;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]==1)
+                continue;
+            flipWindow(nums,i);
+            operations = operations + 1;
+        }
+        if(nums[n-2]==0 or nums[n-1]==0)
+              return -1;
+        return operations;
+    }
+};
+
+// TC = O(N)
+// SC = O(1)
