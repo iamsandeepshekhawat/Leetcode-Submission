e8ee0cc80ed0f5bfdda875796d3659e6c5b2b365
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-04-19 23:44:18 +0530

    Time: 8 ms (5.03%) | Memory: 18.2 MB (5.85%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res ="";
+        for(int i=0;i<strs[0].size();i++){
+            for(auto s:strs){
+                if(s[i]!=strs[0][i]){
+                    return res;
+                }
+            }
+
+            res += strs[0][i];
+        }
+
+         return res;
+
+    }
+
+};
+
+// TC = O(N*M)
+// SC = O(M)
