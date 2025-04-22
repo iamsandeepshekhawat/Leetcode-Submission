5d8ff3ab98f92c4f4d207785fd4093a87c650dfa
Sandeep Singh Shekhawat <87146741+iamsandeepshekhawat@users.noreply.github.com>
2025-04-22 23:55:20 +0530

    Time: 1 ms (16.37%) | Memory: 8.9 MB (21.68%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            char ch = s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    if(ch==')' && st.top()=='(' || ch=='}' && st.top()=='{' ||ch==']' && st.top()=='['){
+                        st.pop();
+    
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }
+
+        }
+
+        if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+// TC = O(N)
+// SC = O(N)
